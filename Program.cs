802d991f817070e394b796d93caf4a5c4fed89c7
fedlferreira <ffylepe@hotmail.com>
2024-10-12 11:07:22 -0300

    Resolução do Exercicio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new("12345", "Maça", "abc", 512);
+Console.WriteLine($"{iphone.Numero}, {iphone.Modelo}, {iphone.IMEI}, {iphone.Memoria}");
+iphone.InstalarAplicativo("Iphone google maps");
+
+Nokia nokia = new("54321", "Tijolao", "bca", 256);
+
+Console.WriteLine($"{nokia.Numero}, {nokia.Modelo}, {nokia.IMEI}, {nokia.Memoria}");
+iphone.InstalarAplicativo("Nokia google maps");
